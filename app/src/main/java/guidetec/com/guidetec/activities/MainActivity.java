530ae13ba05fb9f40c057556a3c6665d27f0629d
Julio Cesar de la Lima <csar_13_lil@hotmail.com>
2018-10-29 19:23:08 -0600

    Funciona Bottom Navigation

--- app/src/main/java/guidetec/com/guidetec/activities/MainActivity.java
+++ app/src/main/java/guidetec/com/guidetec/activities/MainActivity.java
@@ -3,22 +3,45 @@ package guidetec.com.guidetec.activities;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import guidetec.com.guidetec.R;
 import guidetec.com.guidetec.account.LoginActivity;
+import guidetec.com.guidetec.fragments.ARFragment;
+import guidetec.com.guidetec.fragments.MapFragment;
+import guidetec.com.guidetec.fragments.MessageFragment;
+import guidetec.com.guidetec.fragments.PlaceFragment;
+import guidetec.com.guidetec.fragments.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth auth;
     TextView tv;
+    Button btn_logout;
+
+    BottomNavigationView bottomNavigationView;
+
+    private static final String TAG="MainActivity";
+
+    //Fragments
+    MapFragment mapFragment=new MapFragment();
+    PlaceFragment placeFragment=new PlaceFragment();
+    ARFragment arFragment=new ARFragment();
+    MessageFragment messageFragment=new MessageFragment();
+    ProfileFragment profileFragment=new ProfileFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +49,28 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        btn_logout=(Button)findViewById(R.id.btn_logout);
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        //Events
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_augmented);
+
         auth = FirebaseAuth.getInstance();
         //get current user
 
         FirebaseUser currentUser = auth.getCurrentUser();
         tv=(TextView)findViewById(R.id.tv);
         if(currentUser!=null)
-            tv.setText(currentUser.getEmail());
+        {}//tv.setText(currentUser.getEmail());
         else{
             Intent intent =new Intent(MainActivity.this,LoginActivity.class);
             startActivity(intent);
             finish();
         }
+
+
         /*
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         authListener = new FirebaseAuth.AuthStateListener() {
@@ -52,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };*/
+
     }
     // this listener will be called when there is change in firebase user session
     FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
@@ -81,5 +115,29 @@ public class MainActivity extends AppCompatActivity {
         //if (authListener != null) {
           //  auth.removeAuthStateListener(authListener);
         //}
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.navigation_map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,mapFragment).commit();
+
+                return true;
+            case R.id.navigation_place:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,placeFragment).commit();
+                return true;
+            case R.id.navigation_augmented:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,arFragment).commit();
+                return true;
+            case R.id.navigation_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,messageFragment).commit();
+                return true;
+            case R.id.navigation_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,profileFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
