d5ccf997807208e4d71e20c6936ec980f1fb8ff1
Julio Cesar de la Lima <csar_13_lil@hotmail.com>
2018-10-29 23:53:18 -0600

    Commit 201810291152

--- app/src/main/java/guidetec/com/guidetec/activities/MainActivity.java
+++ app/src/main/java/guidetec/com/guidetec/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,6 +17,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
 
 import guidetec.com.guidetec.R;
 import guidetec.com.guidetec.account.LoginActivity;
@@ -36,11 +43,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private static final String TAG="MainActivity";
 
     //Fragments
-    MapFragment mapFragment=new MapFragment();
+    MapFragment mapFragment2=new MapFragment();
     PlaceFragment placeFragment=new PlaceFragment();
     ARFragment arFragment=new ARFragment();
     MessageFragment messageFragment=new MessageFragment();
     ProfileFragment profileFragment=new ProfileFragment();
+
+    // Create fragment
+    final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+    // Create supportMapFragment
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +81,28 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             Intent intent =new Intent(MainActivity.this,LoginActivity.class);
             startActivity(intent);
             finish();
+        }
+
+        //Get instance of mapbox
+        Mapbox.getInstance(this, getString(R.string.mapboxToken));
+
+        if (savedInstanceState == null) {
+            LatLng patagonia = new LatLng(-52.6885, -70.1395);
+            // Build mapboxMap
+            MapboxMapOptions options = new MapboxMapOptions();
+            options.styleUrl(Style.SATELLITE);
+            options.camera(new CameraPosition.Builder()
+                    .target(patagonia)
+                    .zoom(9)
+                    .build());
+
+            // Create map fragment
+            mapFragment = SupportMapFragment.newInstance(options);
+            //mapFragment.get
+
+
+        } else {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("com.mapbox.map");
         }
 
 
@@ -122,8 +157,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         switch (item.getItemId()){
             case R.id.navigation_map:
+                // Add map fragment to parent container
+                //transaction.add(R.id.main_container, mapFragment, "com.mapbox.map");
+                //transaction.commit();
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_container,mapFragment).commit();
-
                 return true;
             case R.id.navigation_place:
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_container,placeFragment).commit();
--- app/src/main/java/guidetec/com/guidetec/fragments/MapFragment.java
+++ app/src/main/java/guidetec/com/guidetec/fragments/MapFragment.java
@@ -2,10 +2,15 @@ package guidetec.com.guidetec.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
 
 import guidetec.com.guidetec.R;
 
@@ -14,17 +19,69 @@ import guidetec.com.guidetec.R;
  */
 public class MapFragment extends Fragment {
 
+    private MapView mapView;
 
     public MapFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view=inflater.inflate(R.layout.fragment_map, container, false);
+
+        Mapbox.getInstance(getActivity(), "pk.eyJ1IjoiZ3VpZGV0ZWMiLCJhIjoiY2ptbWo0aTUyMDI5ZjNrbHVxYnFob29uaSJ9.DVPv3_Q8H_2-oDr784OYug");
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        return  view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 }
